85cdefc24fb05398e6220740d40fbbcc09ec7595
Richard Cummings <richardcummings@Richards-MacBook-Pro.local>
2018-08-12 17:53:04 -0400

    Lesson 24

---
+++ Smack/Model/Message.swift
@@ -0,0 +1,19 @@
+//
+//  Message.swift
+//  Smack
+//
+//  Created by Richard Cummings on 2018-08-12.
+//  Copyright © 2018 Richard Cummings. All rights reserved.
+//
+
+import Foundation
+
+struct Message {
+    public private(set) var message: String!
+    public private(set) var userName: String!
+    public private(set) var channelId: String!
+    public private(set) var userAvatar: String!
+    public private(set) var userAvatarColor: String!
+    public private(set) var id: String!
+    public private(set) var timeStamp: String!
+}
