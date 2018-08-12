450033781182f4335adc1e1f3d62f09e82bf6981
Richard Cummings <richardcummings@Richards-MacBook-Pro.local>
2018-08-12 14:22:17 -0400

    Lesson 19

---
+++ Smack/Model/Channel.swift
@@ -0,0 +1,15 @@
+//
+//  Channel.swift
+//  Smack
+//
+//  Created by Richard Cummings on 2018-08-12.
+//  Copyright © 2018 Richard Cummings. All rights reserved.
+//
+
+import Foundation
+
+struct Channel {
+    public private(set) var channelTitle: String!
+    public private(set) var channelDescription: String!
+    public private(set) var id: String!
+}
